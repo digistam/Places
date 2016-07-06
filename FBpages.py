ef79a41a418b93565a7dcdf3993191a79be768c5
Mark Stam <digistam@gmail.com>
2016-07-06 17:40:42 +0200

    Update FBpages.py
    included subcomments (comments on comments)

--- FBpages.py
+++ FBpages.py
@@ -20,7 +20,7 @@ outfile_path = 'feed_%i%i%i%i%i.csv' % (now.year,now.month,now.day,now.hour,now.
 def write_csv_header():
 	outfile_path = 'feed_%i%i%i%i%i.csv' % (now.year,now.month,now.day,now.hour,now.minute)
 	# open it up, the w means we will write to it
-	with open(outfile_path, 'w') as csvfile:
+	with open(outfile_path, 'a') as csvfile:
 		writer = csv.writer(csvfile, delimiter='|')
 		headers = ['object_id','type','object_name','post_id','post_url','actor','actor_url','actor_id','actor_pic','date','message','story','link','description','comments','likes','application']
 		writer.writerow(headers)
@@ -28,11 +28,11 @@ def write_csv_header():
 def parse_stream(object_id):
 	try:
 	  # open it up, the a means we will append to it
-    with open(outfile_path, 'a') as csvfile:
-	    writer = csv.writer(csvfile, delimiter='|')
-      headers = ['object_id','type','object_name','post_id','post_url','actor','actor_url','actor_id','actor_pic','date','message','story','link','picture','description','comments','likes','application']
-	    writer.writerow(headers)
-			url = requests.get('https://graph.facebook.com/v2.6/%s?fields=name,feed.limit(15){from,created_time,comments,message,story,permalink_url,full_picture,story_tags,link,description,type}&access_token=%s' % (object_id,access_token))
+		with open(outfile_path, 'a') as csvfile:
+			writer = csv.writer(csvfile, delimiter='|')
+      			headers = ['object_id','type','object_name','post_id','post_url','actor','actor_url','actor_id','actor_pic','date','message','story','link','picture','description','comments','likes','application']
+	    		writer.writerow(headers)
+			url = requests.get("https://graph.facebook.com/v2.6/%s?fields=name,feed.limit(15){from,created_time,comments{from,created_time,comments,message,story,permalink_url,full_picture,story_tags,link,description,type},message,story,permalink_url,full_picture,story_tags,link,description,type}&access_token=%s&date_format=Y-m-d H:i:s" % (object_id,access_token))
 			parsed_json = url.json()
 			dict = []
 			likedict = []
@@ -122,9 +122,46 @@ def parse_stream(object_id):
 						dict.append(row)
 						writer.writerow(row)
 						print row
-				
 				except KeyError, e:
-					e
+					print e				
+				except ValueError, e:
+					print e
+				except IndexError, e:
+					print e
+				try:
+					for i in range(len(item['comments']['data'])):
+						for j in range(len(item['comments']['data'][i]['comments']['data'])):
+							row = []
+							row.append(object_id)
+							row.append('subcomment')
+							row.append(parsed_json['name'])
+		                                        row.append(item['comments']['data'][i]['comments']['data'][j]['id'])
+                                                	row.append(item['permalink_url'])
+                                                	row.append(unicode(item['comments']['data'][i]['comments']['data'][j]['from']['name']).encode('utf-8'))
+                                                	row.append('http://www.facebook.com/' + item['comments']['data'][i]['comments']['data'][j]['from']['id'])
+                                                	row.append(item['comments']['data'][i]['comments']['data'][j]['from']['id'])
+		                                        row.append('http://graph.facebook.com/' + item['comments']['data'][i]['comments']['data'][j]['from']['id'] + '/picture')
+                                                	row.append(item['comments']['data'][i]['comments']['data'][j]['created_time'])
+                                                	row.append((unicode(item['comments']['data'][i]['comments']['data'][j]['message']).encode('utf-8')).replace('|',' '))
+  			                                row.append('')
+                                                	row.append('')
+                                                	if item.has_key("full_picture"):
+                                                        	row.append(item["full_picture"])
+                                                	else:
+                                                        	row.append('')
+                                                	row.append('')
+                                                	row.append('')
+                                                	row.append('')
+                                                	row.append('application')
+                                                	dict.append(row)
+							writer.writerow(row)
+							print row		
+				except ValueError, e:
+					print e
+				except KeyError, e:
+					print e
+				except IndexError, e:
+					print e
 	except KeyError, e:
        		print "KeyError: %s" % e
 	except ValueError, e:
@@ -137,29 +174,23 @@ def fill_fusiontable(outfile_path):
     # Procedure to dump CSV file to Fusion Tables
 	try:
 		# Here's the file you get from API Console -> Service Account.
-    f = file('***.p12', 'rb')
-	  key = f.read()
-	  f.close()
-
-	  # Create an httplib2.Http object to handle our HTTP requests and authorize it
-	  # with the Credentials. Note that the first parameter, service_account_name,
-	  # is the Email address created for the Service account. It must be the email
-	  # address associated with the key that was created.
-	  credentials = SignedJwtAssertionCredentials(
-	        '***@***.iam.gserviceaccount.com',
+		f = file('**.p12', 'rb')
+		key = f.read()
+	  	f.close()
+	  	# Create an httplib2.Http object to handle our HTTP requests and authorize it
+	  	# with the Credentials. Note that the first parameter, service_account_name,
+	  	# is the Email address created for the Service account. It must be the email
+	  	# address associated with the key that was created.
+	  	credentials = SignedJwtAssertionCredentials(
+	        '**.iam.gserviceaccount.com',
 	        key,
-	  scope='https://www.googleapis.com/auth/fusiontables')
-	  http = httplib2.Http()
-	  http = credentials.authorize(http)
+	  	scope='https://www.googleapis.com/auth/fusiontables')
+	  	http = httplib2.Http()
+	  	http = credentials.authorize(http)
 
-	  service = build("fusiontables", "v2", http=http)
-	        # For example, let make SQL query to SELECT ALL from Table with
-	        # id = 1gvB3SedL89vG5r1128nUN5ICyyw7Wio5g1w1mbk
-	  media_body = MediaFileUpload(filename=outfile_path, mimetype="application/octet-stream")
-		# print service.table().importRows(tableId='1DT2tM2bsPcAo5TTUoYt8QVR02du8PI0eVCZe3swm', media_body=media_body, encoding="auto-detect", delimiter="|",startLine=1,	isStrict=True).execute()
-		# importRows -> add new rows to existing table
+	  	service = build("fusiontables", "v2", http=http)
+	        media_body = MediaFileUpload(filename=outfile_path, mimetype="application/octet-stream")
 		print service.table().replaceRows(tableId='1DT2tM2bsPcAo5TTUoYt8QVR02du8PI0eVCZe3swm', media_body=media_body, encoding="auto-detect", delimiter="|",startLine=1,	isStrict=True).execute()
-		# replaceRows -> overwrite existing rows in table
 	except ValueError, e:
 		print 'er is iets mis gegaan:',e
 	except errors.HttpError, e:
